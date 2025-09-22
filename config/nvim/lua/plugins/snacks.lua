fe0641dfc4321c01183d545c72579b1060fd8ef1
Kevin Andrus <kevin.andrus@gmail.com>
2025-09-22 21:19:52 +0000

    install snacks, port over goto

---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,125 @@
+telescope = require("telescope.builtin")
+telescope_opts = {layout_strategy="horizontal", layout_config = {height = 0.9, width = 0.9}}
+pick_from_nvim_config = function() 
+  telescope.find_files(
+    {
+      cwd = vim.fn.stdpath("config"),
+      layout_strategy="horizontal",
+      layout_config = {height = 0.9, width = 0.9}
+    }
+  )
+end
+pick_from_dotfiles = function()
+  telescope.find_files(
+    {
+      cwd = "~/code/dotfiles",
+      layout_strategy="horizontal",
+      layout_config = {height = 0.9, width = 0.9},
+    }
+  )
+end
+
+
+---@class snacks.dashboard.Config
+---@field enabled? boolean
+---@field sections snacks.dashboard.Section
+---@field formats table<string, snacks.dashboard.Text|fun(item:snacks.dashboard.Item, ctx:snacks.dashboard.Format.ctx):snacks.dashboard.Text>
+dashboard = {
+  width = 60,
+  row = nil, -- dashboard position. nil for center
+  col = nil, -- dashboard position. nil for center
+  pane_gap = 4, -- empty columns between vertical panes
+  autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", -- autokey sequence
+  -- These settings are used by some built-in sections
+  preset = {
+    -- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
+    ---@type fun(cmd:string, opts:table)|nil
+    pick = nil,
+    -- Used by the `keys` section to show keymaps.
+    -- Set your custom keymaps here.
+    -- When using a function, the `items` argument are the default keymaps.
+    ---@type snacks.dashboard.Item[]
+    keys = {
+      { icon = " ", key = "f", desc = "Find File",
+        action = function() telescope.find_files(telescope_opts) end
+      },
+      { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+      { icon = " ", key = "g", desc = "Find Text",
+        action = function() telescope.live_grep(telescope_opts) end
+      },
+      { icon = " ", key = "r", desc = "Recent Files",
+        action = function() telescope.oldfiles(telescope_opts) end
+      },
+      { icon = " ", key = "c", desc = "Config", action = pick_from_nvim_config },
+      { icon = " ", key = "d", desc = "Dotfiles", action = pick_from_dotfiles },
+      { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+      { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+      { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+    },
+    -- Used by the `header` section
+    header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+  },
+  -- item field formatters
+  formats = {
+    icon = function(item)
+      if item.file and item.icon == "file" or item.icon == "directory" then
+        return M.icon(item.file, item.icon)
+      end
+      return { item.icon, width = 2, hl = "icon" }
+    end,
+    footer = { "%s", align = "center" },
+    header = { "%s", align = "center" },
+    file = function(item, ctx)
+      local fname = vim.fn.fnamemodify(item.file, ":~")
+      fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
+      if #fname > ctx.width then
+        local dir = vim.fn.fnamemodify(fname, ":h")
+        local file = vim.fn.fnamemodify(fname, ":t")
+        if dir and file then
+          file = file:sub(-(ctx.width - #dir - 2))
+          fname = dir .. "/…" .. file
+        end
+      end
+      local dir, file = fname:match("^(.*)/(.+)$")
+      return dir and { { dir .. "/", hl = "dir" }, { file, hl = "file" } } or { { fname, hl = "file" } }
+    end,
+  },
+  sections = {
+    { section = "header" },
+    { section = "keys", gap = 1, padding = 1 },
+    { section = "startup" },
+  },
+}
+
+
+return {
+  "folke/snacks.nvim",
+  priority = 999,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = dashboard,
+    explorer = { enabled = false },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = { enabled = false },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = false },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = false },
+    words = { enabled = true },
+    git = { enabled = true},
+    gitbrowse = { enabled = true},
+  },
+}
