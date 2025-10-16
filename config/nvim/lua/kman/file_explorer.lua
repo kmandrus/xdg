02627c699ce3467747d363fba9d3f085d563de5f
Kevin Andrus <kevin.andrus@flatiron.com>
2025-10-16 17:38:52 -0400

    remove default oil.nvim keymaps

--- config/nvim/lua/kman/file_explorer.lua
+++ config/nvim/lua/kman/file_explorer.lua
@@ -76,7 +76,7 @@ require("oil").setup({
     ["g\\"] = { "actions.toggle_trash", mode = "n" },
   },
   -- Set to false to disable all of the above keymaps
-  use_default_keymaps = true,
+  use_default_keymaps = false,
   view_options = {
     -- Show files and directories that start with "."
     show_hidden = true,
