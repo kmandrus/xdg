f237cf5f48adaa133bc6ff28245afed4be79c5c6
Kevin Andrus <kevin.andrus@flatiron.com>
2025-12-10 12:22:14 -0500

    remove wrap settings for txt and md files

--- config/nvim/ftplugin/text.lua
+++ config/nvim/ftplugin/text.lua
@@ -1,10 +1,10 @@
 vim.opt_local.spell = true
 
-vim.opt_local.wrap = true
-vim.opt_local.linebreak = true
-vim.opt_local.columns = 80
+-- vim.opt_local.wrap = true
+-- vim.opt_local.linebreak = true
+-- vim.opt_local.columns = 80
 
 -- disable options that would override the soft wrap behavior
-vim.opt_local.textwidth = 0
-vim.opt_local.wrapmargin = 0
+-- vim.opt_local.textwidth = 0
+-- vim.opt_local.wrapmargin = 0
 
