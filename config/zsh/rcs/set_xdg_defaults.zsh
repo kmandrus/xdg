f7fb9edf98d1eff7a04a2f5cb2a7231eacc8e4ae
Kevin Andrus <kevin.andrus@gmail.com>
2026-05-20 02:09:42 -0400

    fix bad substitution

--- config/zsh/rcs/set_xdg_defaults.zsh
+++ config/zsh/rcs/set_xdg_defaults.zsh
@@ -1,4 +1,4 @@
 # XDG DEFAULTS
-export XDG_CONFIG_HOME="${$HOME}/code/xdg/config"
+export XDG_CONFIG_HOME="${HOME}/code/xdg/config"
 export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 export XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
