e6e4c6d52ffa7406cb41274345dc0aebf0e9dfff
Kevin Andrus <kevin.andrus@flatiron.com>
2026-09-25 14:28:10 -0400

    add install to fix treesitter parsers

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -4,5 +4,13 @@
       lazy = false,
       build = ":TSUpdate",
       branch = "main",
+      config = function()
+        local ts = require("nvim-treesitter")
+        ts.setup()
+        ts.install({
+          "bash", "c", "zsh", "json", "lua", "markdown", "markdown_inline",
+          "python", "query", "sql", "toml", "vim", "vimdoc", "yaml",
+        })
+      end,
     },
   }
