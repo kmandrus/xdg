3a86c6abdb16d9ee7608d4e8b01274e5392b2eae
Kevin Andrus <kevin.andrus@flatiron.com>
2026-09-25 15:17:01 -0400

    setup work-around for rosetta issues on work macbook

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,15 +2,26 @@
     {
       "nvim-treesitter/nvim-treesitter",
       lazy = false,
-      build = ":TSUpdate",
       branch = "main",
-      config = function()
-        local ts = require("nvim-treesitter")
-        ts.setup()
-        ts.install({
-          "bash", "c", "zsh", "json", "lua", "markdown", "markdown_inline",
-          "python", "query", "sql", "toml", "vim", "vimdoc", "yaml",
-        })
-      end,
+      -- The :TSUpdate build and require('nvim-treesitter').install below are
+      -- disabled: CommandLineTools 27 ships libxcrun.dylib as arm64-only,
+      -- which breaks tree-sitter-cli's build path for our x86_64-Rosetta nvim.
+      -- Parsers and queries under ~/.local/share/nvim/site/ are managed by
+      -- the `ts-install` shell script (config/zsh/scripts/ts-install in this
+      -- repo). Re-enable the block below once we're off Rosetta.
+      -- build = ":TSUpdate",
+      -- config = function()
+      --   local ts = require("nvim-treesitter")
+      --   ts.setup()
+      --   ts.install({
+      --     "bash", "c", "cpp", "css", "diff", "dockerfile",
+      --     "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore",
+      --     "go", "html", "javascript", "json", "lua", "luadoc", "luap",
+      --     "make", "markdown", "markdown_inline",
+      --     "python", "query", "regex", "rst", "ruby", "rust",
+      --     "sql", "terraform", "toml", "tsx", "typescript",
+      --     "vim", "vimdoc", "yaml",
+      --   })
+      -- end,
     },
   }
